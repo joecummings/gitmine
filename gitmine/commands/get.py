969ea965f274eba1ecdd8f6260d7c51af9aaa977
Joe Cummings <jrcummings27@gmail.com>
2020-07-26 22:07:33 -0500

    Reformat into GithubElements class

--- gitmine/commands/get.py
+++ gitmine/commands/get.py
@@ -5,97 +5,97 @@ from gitmine.utils import catch_bad_responses
 import requests
 from collections import defaultdict
 
+import pprint
 
-def _elasped_time_to_color(elapsed_time: timedelta) -> str:
-    if elapsed_time < timedelta(days=1):
-        return "green"
-    if elapsed_time < timedelta(days=3):
-        return "yellow"
-    return "red"
-
-
-def make_hyperlink(link: str, text: str) -> str:
-    """ TODO: actual make this hyperlink
+class GithubElement:
+    """ Container for Github Issue or Pull Request
     """
-    return text
+
+    def __init__(
+        self, type: str, title: str, number: int, url: str, elapsed_time: timedelta
+    ) -> None:
+        self.type = type
+        self.title = title
+        self.number = number
+        self.url = url
+        self.elapsed_time = elapsed_time
+
+    def __repr__(self) -> str:
+        return f"{click.style(''.join(['#', str(self.number)]), fg=self._elapsed_time_to_color())} {self.title}"
+
+    def _elapsed_time_to_color(self) -> str:
+        if self.elapsed_time < timedelta(days=1):
+            return "green"
+        if self.elapsed_time < timedelta(days=3):
+            return "yellow"
+        return "red"
+
+def get_prs(ctx: click.Context, headers: Mapping[str, str]) -> List[Mapping[str, Any]]:
+    """ Get all Github PRs assigned to user.
+    """
+
+    username = ctx.obj.get_value("username")
+    url_format = f"https://api.github.com/search/issues?q=is:open+is:pr+review-requested:{username}"
+    response = requests.get(url_format, headers=headers)
+    catch_bad_responses(response)
+    return response.json()["items"]
 
 
-# @click.pass_context
-# def _get_prs(ctx: click.Context, headers: Mapping[str, str]) -> List[Mapping[str, str]]:
-#     username = ctx.obj.get_value("username")
-#     url_format = f"https://api.github.com/search/issues?q=is:open+is:pr+review-requested:{username}"
-#     response = requests.get(url_format, headers=headers)
-#     catch_bad_responses(response)
-#     return response.json()["items"]
+def get_issues(headers: Mapping[str, str]) -> List[Mapping[str, Any]]:
+    """ Get all Github Issues assigned to user.
+    """
 
-# def _print_prs(prs: List[Mapping[str, str]]) -> None:
-#     click.echo("PR Reviews")
-#     for i, pr in enumerate(prs):
-#         click.echo(pr["number"], color="green")
-#         click.echo(
-#             f"""
-#         {i}: {pr["title"]}
-#         - Url: {pr["html_url"]}
-#         - Time since creation: {datetime.now() - datetime.strptime(pr["created_at"], "%Y-%m-%dT%H:%M:%SZ")}
-#         """
-#         )
-
-
-def _get_issues(headers: Mapping[str, str]) -> List[Mapping[str, Any]]:
     url_format = "https://api.github.com/issues"
     response = requests.get(url_format, headers=headers)
     catch_bad_responses(response)
     return response.json()
 
 
-def _print_issues(issues: List[Mapping[str, Any]]) -> None:
+def print_issues(issues: List[Mapping[str, Any]]) -> None:
+    """ Print issues in the following format:
+
+    repo-title
+    #issue-number issue-title
+    ...
+    """
 
     projects = defaultdict(list)
 
     for issue in issues:
-
         curr_project = issue["repository"]["full_name"]
         projects[curr_project].append(
-            {
-                "title": issue["title"],
-                "number": issue["number"],
-                "url": issue["html_url"],
-                "elapsed_time": datetime.now()
+            GithubElement(
+                type="Issue",
+                title=issue["title"],
+                number=issue["number"],
+                url=issue["html_url"],
+                elapsed_time=datetime.now()
                 - datetime.strptime(issue["created_at"], "%Y-%m-%dT%H:%M:%SZ"),
-            }
+            )
         )
 
     for project, issues in projects.items():
-
         click.echo(project)
-
         for issue in issues:
-
-            color = _elasped_time_to_color(issue["elapsed_time"])
-            formatted_text = make_hyperlink(issue["url"], issue["title"])
-
-            click.echo(
-                f"{click.style(''.join(['#', str(issue['number'])]), fg=color)} {formatted_text}"
-            )
-
+            click.echo(issue)
         click.echo()
 
 
 def get_command(ctx: click.Context, spec: str) -> None:
-    """ Implementation of the *get* command
+    """ Implementation of the *get* command.
     """
 
     headers = {"Authorization": f"Bearer {ctx.obj.get_value('token')}"}
     if spec == "issues":
-        res = _get_issues(headers=headers)
-        _print_issues(res)
+        res = get_issues(headers=headers)
+        print_issues(res)
     elif spec == "prs":
-        # res = _get_prs(headers=headers)
-        # _print_prs(res)
         pass
+        # res = get_prs(ctx, headers=headers)
+        # print_issues(res)
     elif spec == "all":
-        res = _get_issues(headers=headers)
-        _print_issues(res)
+        res = get_issues(headers=headers)
+        print_issues(res)
         click.echo(f"*" * 20)
         # res = _get_prs(headers=headers)
         # _print_prs(res)
--- gitmine/utils.py
+++ gitmine/utils.py
@@ -1,2 +1,5 @@
 def catch_bad_responses(res) -> None:
+    """ Raise error code if response is not 200 OK
+    """
+
     res.raise_for_status()
