09b7c4654d6ee3f0f4a637a1dab48f9e01eb186f
Joe Cummings <jrcummings27@gmail.com>
2020-07-29 14:19:47 -0500

    Start filling out README

---
+++ README.md
@@ -0,0 +1,14 @@
+# gitmine
+
+Stop getting buried under countless Github Issues and PRs. Organize, reference, and close with ease.
+
+Based on the amazing work done by Github themselves on [hub](https://github.com/github/hub).
+
+## Usage
+
+
+## Installation
+
+
+
+## Contributing
--- tests/test_gitmine.py
+++ tests/test_gitmine.py
@@ -4,9 +4,11 @@ from gitmine.commands.config import get_or_create_github_config
 
 runner = CliRunner()
 
+
 def test_config_username_returns_specified():
     result = runner.invoke(config, ["username"])
     assert result.output == ""
+
 
 def test_config_bad_prop_errors():
     result = runner.invoke(config, ["banana-boat"])
