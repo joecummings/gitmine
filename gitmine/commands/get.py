2f5eb8800afcf140b3f07be4c792dcdd664a6122
Joe Cummings <jrcummings27@gmail.com>
2020-08-19 18:12:41 -0500

    try

--- gitmine/commands/get.py
+++ gitmine/commands/get.py
@@ -6,7 +6,7 @@ import time
 from collections import defaultdict
 from datetime import datetime, timedelta
 from itertools import chain
-from typing import Any, List, Mapping
+from typing import Any, List, Mapping, Tuple
 
 import click
 import requests
@@ -102,7 +102,51 @@ class PullRequest(GithubElement):
         )
 
 
-def get_prs(ctx: click.Context, headers: Mapping[str, str]) -> List[Mapping[str, Any]]:
+class Repository:
+    """ Container class for a Github Repository
+    """
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.issues: List[Issue] = []
+        self.prs: List[PullRequest] = []
+
+    def add_issue(self, issue: Issue) -> None:
+        self.issues.append(issue)
+
+    def add_pr(self, pr: PullRequest) -> None:
+        self.prs.append(pr)
+
+    def has_issues(self) -> bool:
+        return True if self.issues else False
+
+    def has_prs(self) -> bool:
+        return True if self.prs else False
+
+    def as_str(self, issues_or_prs: str) -> str:
+        res = [self.name]
+        if issues_or_prs == "issues":
+            for issue in self.issues:
+                res.append(str(issue))
+        elif issues_or_prs == "prs":
+            for pr in self.prs:
+                res.append(str(pr))
+        res.append("\n")
+        return "\n".join(res)
+
+
+class RepoDict(defaultdict):
+    """ Class to extend *defaultdict* to be able to access a key as input
+    """
+
+    def __missing__(self, key):
+        self[key] = Repository(name=key)
+        return self[key]
+
+
+def get_prs(
+    ctx: click.Context, color: bool, headers: Mapping[str, str]
+) -> Tuple[RepoDict, int]:
     """ Get all Github PRs assigned to user.
     """
     username = ctx.obj.get_value("username")
@@ -111,27 +155,15 @@ def get_prs(ctx: click.Context, headers: Mapping[str, str]) -> List[Mapping[str,
     with requests.Session() as s:
         response = s.get(url_format, headers=headers)
     catch_bad_responses(response, get="prs")
-    return response.json()["items"]
+    prs = response.json()["items"]
 
-
-def print_prs(prs: List[Mapping[str, Any]], color: bool, asc: bool, repo: str) -> None:
-    """ Print PRs in the following format:
-
-    repo-title
-    #pr-number pr-title
-    ...
-    """
-    if not prs:
-        click.echo("No assigned PRs! Keep up the good work.")
-
-    projects = defaultdict(list)
-
+    num_of_prs = 0
+    repositories = RepoDict()
     for pr in prs:
+        num_of_prs += 1
         url = pr["html_url"]
-        curr_project = re.findall(r"github.com/(.+?)/pull", url)[0]
-        if repo and curr_project != repo:
-            continue
-        projects[curr_project].append(
+        repo_name = re.findall(r"github.com/(.+?)/pull", url)[0]
+        repositories[repo_name].add_pr(
             PullRequest(
                 title=pr["title"],
                 number=pr["number"],
@@ -142,12 +174,12 @@ def print_prs(prs: List[Mapping[str, Any]], color: bool, asc: bool, repo: str) -
             )
         )
 
-    echo_elements(projects, len(prs))
+    return repositories, num_of_prs
 
 
 def get_unassigned_issues(
-    asc: bool, headers: Mapping[str, str]
-) -> List[Mapping[str, Any]]:
+    asc: bool, color: bool, headers: Mapping[str, str]
+) -> Tuple[Mapping[str, Repository], int]:
     """ Get all Github Issues that are unnassigned from the repos in which user is a collaborator.
     """
 
@@ -164,7 +196,7 @@ def get_unassigned_issues(
     collaborator_repos = get_collaborator_repos()
     params = {"direction": "asc" if asc else "desc", "assignee": "none"}
 
-    def get_issues_by_repo(repo: Mapping[str, Any]) -> List[Mapping[str, Any]]:
+    def get_issues_by_repo(repo: Mapping[str, Any]) -> Repository:
         """ Get all Github Issues in a repo specified by params.
         """
 
@@ -172,55 +204,56 @@ def get_unassigned_issues(
         url = f"https://api.github.com/repos/{repo['full_name']}/issues"
         with session.get(url, headers=headers, params=params) as response:
             catch_bad_responses(response, get="issues")
-            issues = []
+            repo_name = repo["full_name"]
+            repo_class = Repository(name=repo_name)
             for issue in response.json():
-                issue["repository"] = {"full_name": repo["full_name"]}
-                issues.append(issue)
-            return issues
+                repo_class.add_issue(
+                    Issue(
+                        title=issue["title"],
+                        number=issue["number"],
+                        url=issue["html_url"],
+                        elapsed_time=datetime.now()
+                        - datetime.strptime(issue["created_at"], "%Y-%m-%dT%H:%M:%SZ"),
+                        color_coded=True if color else False,
+                    )
+                )
+            return repo_class
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        issues = filter(
-            lambda x: x, executor.map(get_issues_by_repo, collaborator_repos)
-        )
+        repositories = {
+            repo.name: repo
+            for repo in filter(
+                lambda x: x.has_issues(),
+                executor.map(get_issues_by_repo, collaborator_repos),
+            )
+        }
 
-    return list(chain.from_iterable(issues))
+    # there's gotta be a better way to get the num_of_issues, right?
+    return repositories, sum([len(r.issues) for r in repositories.values()])
 
 
 def get_issues(
-    unassigned: bool, asc: bool, headers: Mapping[str, str]
-) -> List[Mapping[str, Any]]:
+    unassigned: bool, asc: bool, color: bool, headers: Mapping[str, str]
+) -> Tuple[Mapping[str, Repository], int]:
     """ Get all Github Issues assigned to user.
     """
 
     if unassigned:
         click.echo("Hang on, getting unassigned issues for you...")
-        return get_unassigned_issues(asc, headers)
+        return get_unassigned_issues(asc, color, headers)
 
     params = {"direction": "asc" if asc else "desc"}
     logger.debug(f"Fetching issues from github.com \n")
     url_format = "https://api.github.com/issues"
     response = requests.get(url_format, headers=headers, params=params)
     catch_bad_responses(response, get="issues")
-    return response.json()
 
-
-def print_issues(issues: List[Mapping[str, Any]], color: bool, repo: str) -> None:
-    """ Print issues in the following format:
-
-    repo-title
-    #issue-number issue-title
-    ...
-    """
-    if not issues:
-        click.echo("No assigned Issues! Keep up the good work.")
-
-    projects = defaultdict(list)
-
-    for issue in issues:
-        curr_project = issue["repository"]["full_name"]
-        if repo and curr_project != repo:
-            continue
-        projects[curr_project].append(
+    repositories = RepoDict()
+    num_of_issues = 0
+    for issue in response.json():
+        num_of_issues += 1
+        repo_name = issue["repository"]["full_name"]
+        repositories[repo_name].add_issue(
             Issue(
                 title=issue["title"],
                 number=issue["number"],
@@ -230,29 +263,28 @@ def print_issues(issues: List[Mapping[str, Any]], color: bool, repo: str) -> Non
                 color_coded=True if color else False,
             )
         )
+    return repositories, num_of_issues
 
-    echo_elements(projects, len(issues))
 
+def echo_info(
+    repos: Mapping[str, Repository], type_of_data: str, num_of_issues: int
+) -> None:
+    """ Print issues/prs in the following format:
 
-def echo_elements(projects: Mapping[str, Any], num_of_issues: int) -> None:
-    """ Print to stdout.
+    repo-title
+    #issue-number issue-title
+    ...
     """
 
+    if not repos:
+        click.echo(f"No {type_of_data} found! Keep up the good work.")
+
     if num_of_issues > 20:
-        joined_str = []
-        for project, elements in projects.items():
-            joined_str.append(project)
-            for elem in elements:
-                joined_str.append(str(elem))
-            joined_str.append("\n")
-        join_elements = "\n".join(joined_str)
-        click.echo_via_pager(join_elements)
+        all_repos = [repo.as_str(type_of_data) for repo in repos.values()]
+        click.echo_via_pager("\n".join(all_repos))
     else:
-        for project, elements in projects.items():
-            click.echo(project)
-            for element in elements:
-                click.echo(element)
-            click.echo()
+        for repo in repos.values():
+            click.echo(repo.as_str(type_of_data))
 
 
 def get_command(
@@ -267,17 +299,17 @@ def get_command(
     )
     headers = {"Authorization": f"Bearer {ctx.obj.get_value('token')}"}
 
-    if spec == "issues":
-        res = get_issues(unassigned, asc, headers=headers)
-        print_issues(res, color, repo)
-    elif spec == "prs":
-        res = get_prs(ctx, headers=headers)
-        print_prs(res, color, asc, repo)
-    elif spec == "all":
-        res = get_issues(unassigned, asc, headers=headers)
-        print_issues(res, color, repo)
+    if spec == "all":
+        res, num_of_issues = get_issues(unassigned, asc, color, headers=headers)
+        echo_info(res, "issues", num_of_issues)
         click.echo(f"* " * 20)
-        res = get_prs(ctx, headers=headers)
-        print_prs(res, color, asc, repo)
+        res, num_of_prs = get_prs(ctx, color, headers=headers)
+        echo_info(res, "prs", num_of_prs)
+    elif spec == "issues":
+        res, num_of_issues = get_issues(unassigned, asc, color, headers=headers)
+        echo_info(res, "issues", num_of_issues)
+    elif spec == "prs":
+        res, num_of_prs = get_prs(ctx, color, headers=headers)
+        echo_info(res, "prs", num_of_prs)
     else:
         raise click.BadArgumentUsage(message=f"Unkown spec: {spec}")
